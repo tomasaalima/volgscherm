cf68abbef4292a3c5751ece0d804c274ed783404
tomasaalima <tomaslima_20@outlook.com>
2022-11-28 17:27:57 -0300

    #

--- assets/script/home.php
+++ assets/script/home.php
@@ -12,30 +12,96 @@
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+    google.charts.load('current', {packages: ['corechart', 'line']});
+    google.charts.setOnLoadCallback(drawBasic);
 
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2013',  1000,      400],
-          ['2014',  1170,      460],
-          ['2015',  660,       1120],
-          ['2016',  1030,      540]
+    function drawBasic() {
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'X');
+        data.addColumn('number', 'Dogs');
+
+        data.addRows([
+            [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+            [6, 11],  [7, 27],  [8, 33]
         ]);
 
         var options = {
-          title: 'Company Performance',
-          hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
-          vAxis: {minValue: 0},
-          backgroundColor: 'black'
+            hAxis: {
+            title: 'Time'
+            },
+            vAxis: {
+            title: 'Popularity'
+            },
+            backgroundColor: 'transparent'
         };
 
-        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-    </script>
+        var chart = new google.visualization.LineChart(document.getElementById('line-chart'));
 
+        chart.draw(data, options);
+        }
+    </script>
+    <script>
+        google.charts.load('current', {packages: ['corechart', 'bar']});
+        google.charts.setOnLoadCallback(drawBasic);
+
+        function drawBasic() {
+
+            var data = new google.visualization.DataTable();
+            data.addColumn('timeofday', 'Time of Day');
+            data.addColumn('number', 'Motivation Level');
+
+            data.addRows([
+                [{v: [8, 0, 0], f: '8 am'}, 1],
+                [{v: [9, 0, 0], f: '9 am'}, 2],
+                [{v: [10, 0, 0], f:'10 am'}, 3]
+            ]);
+
+            var options = {
+                title: 'Motivation Level Throughout the Day',
+                hAxis: {
+                title: 'Time of Day',
+                format: 'h:mm a',
+                viewWindow: {
+                    min: [7, 30, 0],
+                    max: [17, 30, 0]
+                }
+                },
+                vAxis: {
+                title: 'Rating (scale of 1-10)'
+                },
+                backgroundColor: 'transparent'
+            };
+
+            var chart = new google.visualization.ColumnChart(
+                document.getElementById('bar-chart'));
+
+            chart.draw(data, options);
+        }
+    </script>
+    <script>
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Work',     11],
+            ['Eat',      2],
+            ['Commute',  2],
+            ['Watch TV', 2],
+            ['Sleep',    7]
+            ]);
+
+            var options = {
+            title: 'My Daily Activities',
+            is3D: true,
+            backgroundColor: 'transparent'
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('pie-chart'));
+            chart.draw(data, options);
+        }
+    </script>
 </head>
 <body>
     <?php
@@ -62,8 +128,11 @@
         </div>
         <div class="main-dashboard">
         
-            <div class="dashboard-graphic">
-            <div id="chart_div" style="width: 100%; height: 500px;"></div>
+            <div id="line-chart" style="width: 100%; height: 500px;"></div>
+            <div id="bar-chart" style="width: 100%; height: 500px;"></div>
+            <div>
+                <div id="pie-chart" style="width: 100%; height: 500px;"></div>
+                
             </div>
             
         </div>
