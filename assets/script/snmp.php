96d07ab045b92110479eec3fb4419c3fd6b6d63e
tomasaalima <tomaslima_20@outlook.com>
2022-11-30 13:41:33 -0300

    #

--- assets/script/snmp.php
+++ assets/script/snmp.php
@@ -1,19 +1,76 @@
 <?php
-    $init = "172.26.100.173";
-    $end = "172.26.255.255";
+    require("db_connection.php");
 
-    $teste = explode(".", $init);
+    $init = "172.26.129.170";
+    $end = "172.26.129.174";
     
+    while(runRange($init, $end)){
+        $init = runRange($init, $end);
 
-    $iso = shell_exec("wsl snmpget -Oqv -v2c -c public 172.26.129.173 iso.3.6.1.2.1.1.4.0");
+        $iso = shell_exec("wsl snmpget -Oqv -v2c -c public $init iso.3.6.1.2.1.1.4.0");
 
-    echo $iso."<br>";
-/*
-    function runRange($rangeA, $rangeB){
-        while($rangeA){
+        /*capturar serial*/
 
+        if($iso === null){
+            echo "null";
+        }else{
+            registerPrinter("1",$init);
+            echo "Dado captado".$iso;
         }
-    }*/
+        echo "<br>";
+        
+    }
+
+    function runRange($rangeA, $rangeB){
+
+        if($rangeA == $rangeB){
+            return false;
+        }else{
+            $A = explode(".", $rangeA);
+            $B = explode(".", $rangeB);
+
+            if($A[3] == 255 and $A[2] == 255 and $A[1] == 255){
+
+                $A[0] += 1;
+                $A[1] = 0;
+                $A[2] = 0;
+                $A[3] = 0;
+
+                return implode(".", $A);
+            }else if ($A[3] == 255 and $A[2] == 255){
+            
+                $A[1] += 1;
+                $A[2] = 0;
+                $A[3] = 0;
+
+                return implode(".", $A);
+            }else if ($A[3] == 255){
+                $A[2] += 1;
+                $A[3] = 0;
+
+                return implode(".", $A);
+            }else{
+                $A[3] += 1;
+
+                return implode(".", $A);
+            }  
+        }
+    }
+
+    function registerPrinter($serial, $ip){
+
+        $sql = "SELECT * FROM impressora WHERE serial = '$serial'";
+        global $connection;
+        $result = $connection->query($sql) or die("Falha na execução do código SQL"). $connection->error;
+
+        $quantidade = $result->num_rows;
+
+            if($quantidade == 1){
+                $connection->query("UPDATE impressora SET endereco_ip = '$ip' WHERE serial = '$serial'") or die("Falha na execução do código SQL"). $connection->error;
+            }else{
+                
+            }
+    }
 
     /*
     include("db_connection.php");
--- assets/script/teste.php
+++ assets/script/teste.php
@@ -1,6 +1,47 @@
 <?php
+ $init = "172.26.120.10";
+ $end = "172.26.140.19";
+ 
+while(runRange($init, $end)){
+    $init = runRange($init, $end);
 
-$init = "172.26.100.173";
 
-$teste = explode(".", $init);
-echo $teste[3];
+    echo $init,"<br>";
+    echo $end."<br><br>";
+}
+
+function runRange($rangeA, $rangeB){
+
+    if($rangeA == $rangeB){
+        return false;
+    }else{
+        $A = explode(".", $rangeA);
+        $B = explode(".", $rangeB);
+
+        if($A[3] == 255 and $A[2] == 255 and $A[1] == 255){
+
+            $A[0] += 1;
+            $A[1] = 0;
+            $A[2] = 0;
+            $A[3] = 0;
+
+            return implode(".", $A);
+        }else if ($A[3] == 255 and $A[2] == 255){
+           
+            $A[1] += 1;
+            $A[2] = 0;
+            $A[3] = 0;
+
+            return implode(".", $A);
+        }else if ($A[3] == 255){
+            $A[2] += 1;
+            $A[3] = 0;
+
+            return implode(".", $A);
+        }else{
+            $A[3] += 1;
+
+            return implode(".", $A);
+        }  
+    }
+}
